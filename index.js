5a80dc65ef204dd1c377125360ea53099c5b18cd
tohhongxiang123 <tohhongxiang@gmail.com>
2019-09-18 17:37:56 +0800

    Preparing for heroku deployment

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/UserRoute');
 const postRoutes = require('./routes/PostRoute');
 const protectedRoutes = require('./routes/protectedRoute');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -14,10 +15,14 @@ app.use('/api/users/updateuser', express.json({limit: '10mb'})); // changing the
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 // ROUTES
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/protected', protectedRoutes);
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+})
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
